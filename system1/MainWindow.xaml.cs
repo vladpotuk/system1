a140eae15be8de02e73a6bca8ea240f3a95f276e
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-04 21:38:17 +0300

    update

--- system1/MainWindow.xaml.cs
+++ system1/MainWindow.xaml.cs
@@ -1,24 +1,32 @@
-﻿using System.Text;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace system1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void StartProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Process process = new Process();
+            process.StartInfo.FileName = "notepad.exe"; 
+            process.Start();
+
+            
+            await Task.Run(() => process.WaitForExit());
+
+            
+            int exitCode = process.ExitCode;
+
+            
+            ResultTextBlock.Text = $"Process exited with code: {exitCode}";
         }
     }
 }
