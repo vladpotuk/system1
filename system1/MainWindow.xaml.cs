11f24258851075cc134dcf6d4770c1e017f855b5
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-04 19:49:02 +0300

    update

--- system1/MainWindow.xaml.cs
+++ system1/MainWindow.xaml.cs
@@ -1,24 +1,63 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace system1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+        private List<Process> processes;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); 
+            timer.Tick += Timer_Tick;
+
+           
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            
+            processes = Process.GetProcesses().ToList();
+
+           
+            ProcessListView.ItemsSource = processes;
+        }
+
+        private void UpdateIntervalButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               
+                int interval = int.Parse(UpdateIntervalTextBox.Text);
+
+                
+                if (interval <= 0)
+                {
+                    MessageBox.Show("Інтервал оновлення має бути додатнім числом.", "Помилка");
+                    return;
+                }
+
+               
+                timer.Interval = TimeSpan.FromSeconds(interval);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Неправильний формат інтервалу оновлення.", "Помилка");
+            }
         }
     }
 }
